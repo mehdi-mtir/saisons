6991a7ba145b8e3ae24824552197a86bc05dd27a
mehdi-mtir <mehdi.mtir@gmail.com>
2020-12-14 11:42:37 +0100

    version 1 - demo state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+/*const successFunction = (position) => {
+  console.log(position);
+};
+
+const failureFunction = (err) => {
+  console.log(err);
+};
+
+const App = () => {
+  navigator.geolocation.getCurrentPosition(successFunction,failureFunction);
+
+  return <div>Bonjour dans notre application Saisons</div>;
+};
+//
+function getCurrentPosition(f1, f2){
+  //faire le travail de recuperation de la position
+  //...
+  if(success){
+    f1(position)
+  }
+  else{
+    f2(err)
+  }
+}*/
+//let x = 0;
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {lat : null};
+
+    window.navigator.geolocation.getCurrentPosition( //getCurrent position s'exétura en asynchrone
+      (position) => {
+        //this.state.lat ///A ne pas faire
+        this.setState({lat : position.coords.latitude});
+        //!!!! pour modifier la valeur d'un paramètre de notre etat (state), il faut passer par la méthode setState()
+      }, //callback function
+      err => console.log(err) //callback function
+    );
+
+  }
+
+
+  render(){
+    return <div>Latitude : {this.state.lat}</div>;
+  }
+}
+
+/*const App = () => {
+  let latitude = 0;
+  navigator.geolocation.getCurrentPosition( //getCurrent position s'exétura en asynchrone
+    position => {
+      latitude = position.coords.latitude;
+      console.log(latitude);
+    }, //callback function
+    err => console.log(err) //callback function
+  );
+
+  return <div>Latitude : {latitude}</div>;
+};
+*/
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />,
+  document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
